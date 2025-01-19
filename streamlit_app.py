012975f0038e2ef662eda0b48495cff81dd14c0f
rishabh-vr <157272152+rishabh-vr@users.noreply.github.com>
2025-01-19 20:40:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("Fine-Tuning BERT for Movie-Review Sentiment Classification")
 
 classifier = pipeline('text-classification', model='bert-base-uncased-sentiment-model')
 
-text = st.text_area("Enter Your Tweet Here")
+text = st.text_area("Enter Your Review Here")
 
 if st.button("Predict"):
         result = classifier(text)
